60f725f7fa0a8c561f0804bc662bbdcec06c0391
Lucky <470667444@qq.com>
2017-05-25 10:51:31 +0800

    重新整理文件

--- Assets/CSVSystem/Script/NewVersion/Singleton.cs
+++ Assets/CSVSystem/Script/NewVersion/Singleton.cs
@@ -55,7 +55,6 @@ namespace CSVSystem
                         {
                             GameObject singleton = new GameObject();
                             _instance = singleton.AddComponent<T>();
-                            //_instance.SingletonInitialize();
 
                             singleton.name = "(singleton)" + typeof(T).ToString();
 
